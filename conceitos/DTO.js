742c82200a01efbb021676e2d0a36675067d363e
Rafael C <rafasgcarvalho@gmail.com>
2024-07-10 16:04:37 -0300

    Adicionado conceito DTO

---
+++ conceitos/DTO.js
@@ -0,0 +1,27 @@
+function calcularTotalPedido(precoUnitario, quantidade) {
+    return precoUnitario * quantidade
+}
+
+const totalPedido1 = calcularTotalPedido(20, 3);
+
+function calcularTotal({precoUnitario, quantidade}) {
+    return precoUnitario * quantidade;
+}
+
+const totalPedido2 = calcularTotal({
+    precoUnitario: 20,
+    quantidade: 3
+});
+
+const pedidoDTO = {
+    precoUnitario: 20,
+    quantidade: 3
+};
+
+const totalPedido3 = calcularTotal(pedidoDTO);
+
+//repositorio.buscarPorId('123');
+
+console.log(totalPedido1);
+console.log(totalPedido2);
+console.log(totalPedido3);
